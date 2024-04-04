1a75eba6d830bcc1ae8dd0fd1d46aef90cc974d3
haseeb4339 <haseebullahbj@gmail.com>
2024-04-05 03:24:31 +0500

    create a get view

--- Project#1/Shopping/customers/urls.py
+++ Project#1/Shopping/customers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import custormer_list
+from .views import custormer_list, customer_details
 
 
 urlpatterns = [
     path('', custormer_list),
+    path('<pk>/',customer_details ),
 ]
--- Project#1/Shopping/customers/views.py
+++ Project#1/Shopping/customers/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse, JsonResponse
 from rest_framework.parsers import JSONParser
 from .models import Customer
 from .serializers import CustomerSerializer
+from django.views.decorators.csrf import csrf_exempt
 
+
+@csrf_exempt
 def custormer_list(request):
 
     if request.method == "GET":
@@ -28,3 +31,19 @@ def custormer_list(request):
             return JsonResponse(serializer.data , status = 201)
         
         return JsonResponse(serializer.errors, status = 400)
+    
+
+def customer_details(request, pk):
+
+    try:
+
+        customer = Customer.objects.get(id=pk)
+
+    except customer.DoesNotExist:
+        return HttpResponse('not found!')
+    
+    if request.method == "GET":
+
+        serializer = CustomerSerializer(customer)
+
+        return JsonResponse(serializer.data)
