ec75431f9c1693e312d1892bd4037d0eff7f3276
haseeb4339 <haseebullahbj@gmail.com>
2024-04-06 22:27:41 +0500

    create a register logic

--- Project#2/auth/users/views.py
+++ Project#2/auth/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views  import APIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response(serializer.data)
+
