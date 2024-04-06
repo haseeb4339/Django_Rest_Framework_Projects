02a26ae10382ccf53cb0dad5ae46d8263144a944
haseeb4339 <haseebullahbj@gmail.com>
2024-04-06 22:48:38 +0500

    create a login logic

--- Project#2/auth/users/views.py
+++ Project#2/auth/users/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-
+from .models import User
 from rest_framework.views  import APIView
 from .serializers import UserSerializer
 from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 
 class RegisterView(APIView):
     def post(self, request):
@@ -12,4 +13,25 @@ class RegisterView(APIView):
             serializer.save()
 
             return Response(serializer.data)
+        
+
+class LoginView(APIView):
+
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('User not found')
+        
+        if not user.check_password(password):
+            raise AuthenticationFailed('password is incorrect')
+
+
+        return Response({
+            'message': 'successfully login'
+        })
+
 
