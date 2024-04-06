ec4c28502158cf223583584c8f545f0b45856b88
haseeb4339 <haseebullahbj@gmail.com>
2024-04-06 23:45:36 +0500

    successfully added cookie login

--- Project#2/auth/users/views.py
+++ Project#2/auth/users/views.py
@@ -39,13 +39,20 @@ class LoginView(APIView):
         }
 
         token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+
+        response.set_cookie(key='jwt', value = token, httponly = True)
+
+        response.data = {
+            'jwt': token
+
+        }
       
 
         
 
 
-        return Response({
-            'jwt': token
-        })
+        return response
 
 
