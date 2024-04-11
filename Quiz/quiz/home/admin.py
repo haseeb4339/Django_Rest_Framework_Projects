6b1b852670988a113e286929d2f0fffcab7fe326
haseeb4339 <haseebullahbj@gmail.com>
2024-04-11 12:31:40 +0500

    create a logic which display the foreignkey relationship at admin panel

--- Quiz/quiz/home/admin.py
+++ Quiz/quiz/home/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 from .models import Category, Question, Answer
 
 admin.site.register(Category)
-admin.site.register(Question)
+
+class AnswerAdmin(admin.StackedInline):  # it can list all the ForeignKey relationship
+
+    model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdmin]
+
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
